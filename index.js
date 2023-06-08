1f1fb9804c42b5eebd7e84d71d04e076dd5faa31
hritik2899 <103510798+hritik2899@users.noreply.github.com>
2023-06-09 01:10:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -160,3 +160,30 @@ const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`LISTENING ON PORT ${port}`);
 })
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
+
+// Your React component code
+function App() {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="#home">My Website</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#services">Services</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      
+      {/* Your JSX/HTML content */}
+    </div>
+  );
+}
+
+export default App;
