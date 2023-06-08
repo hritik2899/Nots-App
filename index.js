52f19b24394d4f63c7ced9daa39adf1147fbed10
hritik2899 <103510798+hritik2899@users.noreply.github.com>
2023-06-09 01:24:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -164,26 +164,18 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav } from 'react-bootstrap';
 
-// Your React component code
-function App() {
-  return (
-    <div>
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="#home">My Website</Navbar.Brand>
+<Navbar bg="primary" variant="dark" expand="lg">
+        <Navbar.Brand style={{ fontSize: '24px', paddingRight: '20px' }}>
+          <Image src="logo.png" alt="Logo" className="mr-2" />
+          My Website
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar-nav" />
         <Navbar.Collapse id="navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#services">Services</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+            <div className="d-flex align-items-center">
+              <Image src="user.png" alt="User" roundedCircle className="mr-2" style={{ width: '32px' }} />
+              <span style={{ color: 'white' }}>John Doe</span>
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      
-      {/* Your JSX/HTML content */}
-    </div>
-  );
-}
-
-export default App;
